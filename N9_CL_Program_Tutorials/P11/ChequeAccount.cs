b6b803c46244daee8d7688c6b9b59330ef1dcfb7
Evan <szevanzhang@gmail.com>
2025-08-27 12:09:51 +1200

    incooporate code corresponding to Question 5 and Question 7 in P11

---
+++ N9_CL_Program_Tutorials/P11/ChequeAccount.cs
@@ -0,0 +1,25 @@
+namespace N9_CL_Program_Tutorials.P11;
+
+/// <summary>
+/// Question 7 (b)
+/// </summary>
+public class ChequeAccount : BankAccount
+{
+    public ChequeAccount(int acctNum) : base(acctNum, 0)
+    {
+    }
+
+    protected override void Withdraw(decimal amount)
+    {
+        base.Withdraw(amount);
+        if (Balance - amount >= -100) //$100 overdraft in cheque account
+            Balance -= amount;
+        else
+            throw new Exception("Withdraw failed, insufficient funds");
+    }
+
+    public override void AddInterest()
+    {
+        Balance += Balance * 0.03m; //3% interest rate
+    }
+}
---
+++ N9_CL_Program_Tutorials/P11/StockTakingResult.cs
@@ -0,0 +1,11 @@
+namespace N9_CL_Program_Tutorials.P11;
+
+/// <summary>
+/// Question 5 (c)
+/// </summary>
+public enum StockTakingResult
+{
+    Over,
+    Equal,
+    Under
+}
