7744ea3de114b65ddf986d6aa4e2d62a7b67129c
Evan <szevanzhang@gmail.com>
2025-09-02 11:40:05 +1200

    incorporate the practice of TDD using xUnit

---
+++ N9_StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿namespace N9_StringCalculator.Tests;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void Add_EmptyString_ReturnsZero()
+    {
+        // arrange
+        var calculator = new StringCalculator();
+
+        // act
+        var result = calculator.Add("");
+
+        // assert
+        Assert.Equal(0, result);
+    }
+
+
+    [Fact]
+    public void Add_SingleNumber_ReturnsThatNumber()
+    {
+        // arrange
+        var calculator = new StringCalculator();
+
+        // act
+        int result = calculator.Add("1");
+
+        // assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Add_TwoNumbers_ReturnsTheSum()
+    {
+        // Arrange
+        var calculator = new StringCalculator();
+
+        // Act
+        var result = calculator.Add("1,2");
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+}
---
+++ N9_StringCalculator/StringCalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace N9_StringCalculator;
+
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        return numbers == string.Empty ? 0 : numbers.Split(',').Select(int.Parse).Sum();
+    }
+}
