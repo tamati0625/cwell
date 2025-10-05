d0bf67d701ad3fb163d4c05705e548e0f7e80caf
Evan <szevanzhang@gmail.com>
2025-10-06 12:04:13 +1300

    Add a placeholer for regex

---
+++ N9_Library.Tests/Regex/RegexUtilTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using N9_Library.Regex;
+
+namespace N9_Library.Tests.Regex;
+
+[TestClass]
+[TestSubject(typeof(RegexUtil))]
+public class RegexUtilTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
---
+++ N9_Library/Regex/RegexUtil.cs
@@ -0,0 +1,6 @@
+namespace N9_Library.Regex;
+
+public class RegexUtil
+{
+    
+}
