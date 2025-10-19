94f0c281a290cbfd1d06ccd240bd13852c88bc97
Evan <szevanzhang@gmail.com>
2025-10-19 17:24:26 +1300

    test static variables and constants

---
+++ N9_Library/Graphs/Color.cs
@@ -0,0 +1,8 @@
+namespace N9_Library.Graphs;
+
+public enum Color
+{
+    Red,
+    Green,
+    Yellow
+}
--- N9_Library/Graphs/Shape.cs
+++ N9_Library/Graphs/Shape.cs
@@ -1,10 +1,15 @@
+using System.Reflection.Metadata;
+
 namespace N9_Library.Graphs;
 
 public abstract class Shape
 {
+    private const decimal Constant1 = 0;
+    private static decimal Static1;
     public abstract void Method();
 
     public virtual void Method1()
     {
+        Shape.Static1 = 1;
     }
 }
